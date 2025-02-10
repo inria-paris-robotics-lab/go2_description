78a5b5357e0a3472682794eb0860be4b9aa22cc4
Ewen Dantec <ewen.dantec@inria.fr>
2025-02-10 10:45:06 +0100

    Fix typo in loadGo2

--- python/go2_description/loader.py
+++ python/go2_description/loader.py
@@ -2,7 +2,7 @@ import pinocchio as pin
 from .path import *
 
 def loadGo2() -> pin.RobotWrapper:
-    robot = pin.RobotWrapper.BuildFromURDF(GO2_DESCRIPTION_URDF_PATH, [GO2_DESCRIPTION_PACKAGE_DIR], pin.JoinModelFreeFlyer())
+    robot = pin.RobotWrapper.BuildFromURDF(GO2_DESCRIPTION_URDF_PATH, [GO2_DESCRIPTION_PACKAGE_DIR], pin.JointModelFreeFlyer())
 
     # Reference configurations
     pin.loadReferenceConfigurations(robot.model, GO2_DESCRIPTION_SRDF_PATH)
